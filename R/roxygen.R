f9191d8f3f053249e6698ff798f0257d2fdb0f89
rjbgoudie <rjbgoudie@users.noreply.github.com>
2010-12-13 14:09:08 +0000

    Fix document() and add roxygen as a suggests

--- R/roxygen.R
+++ R/roxygen.R
@@ -2,13 +2,16 @@
 #'
 #' A variant of Hadlay Wickham's document() function in devtools.
 #' 
-#' @param pkg A package
+#' @param pkg A package, see \code{\link[devtools]{as.package}}.
 #' @export
 document <- function(pkg) 
 {
-    pkg <- as.package(pkg)
-    require(roxygen)
-    if (exists("roxygenise")) {
-      roxygenize(pkg$path, pkg$path, F, T, T, use.Rd2 = T)
-    }
+  pkg <- as.package(pkg)
+  require(roxygen)
+  roxygenize(package.dir  = pkg$path,
+            roxygen.dir   = pkg$path,
+            copy.package  = F,
+            overwrite     = T,
+            unlink.target = T,
+            use.Rd2       = F)
 }
