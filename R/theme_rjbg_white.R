5a4dc5150c9cb9541b56e7e1fedb56bccdb7355f
rjbgoudie <rjbgoudie@users.noreply.github.com>
2014-08-22 11:06:37 +0100

    Whiten theme_rjbg_white further
    Increase the base font size, but reduce it for axis titles.
    Darken the grid (so that it shows up when printed), but
    make the facet backgrounds white. Reduce the default point
    size

--- R/theme_rjbg_white.R
+++ R/theme_rjbg_white.R
@@ -1,5 +1,5 @@
 #' @export
-theme_rjbg_white <- function(base_size = 8, base_family = "") {
+theme_rjbg_white <- function(base_size = 12, base_family = "") {
   # Starts with theme_grey and then modify some parts
   theme_grey(base_size = base_size, base_family = base_family) %+replace%
   theme(
@@ -11,18 +11,19 @@ theme_rjbg_white <- function(base_size = 8, base_family = "") {
     panel.background  = element_rect(fill = "white", colour = NA),
     panel.border      = element_rect(fill = NA,
                                      colour = "grey80",
-                                     size = 0.5),
-    panel.grid.major  = element_line(colour = "grey94",
+                                     size = 0.4),
+    panel.grid.major  = element_line(colour = "grey85",
                                      size = 0.125),
-    panel.grid.minor  = element_line(colour = "grey97",
+    panel.grid.minor  = element_line(colour = "grey90",
                                      size = 0.075),
-    strip.background  = element_rect(fill = "grey80",
+    strip.background  = element_rect(fill = "white",
                                      size = NA),
     strip.text.x      = element_text(colour = "black"),
     strip.text.y      = element_text(colour = "black",
                                      angle = -90),
+    axis.title         = element_text(size = rel(0.8)),
     legend.key = element_blank(),
-    legend.background = element_rect(fill="transparent")
+    legend.background = element_blank()
     )
 }
 
@@ -35,5 +36,6 @@ colours_rjbg <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
   if (require(ggplot2)){
     update_geom_defaults("line", list(size = 0.2))
     update_geom_defaults("step", list(size = 0.2))
+    update_geom_defaults("point", list(size = 1.5))
   }
 }
